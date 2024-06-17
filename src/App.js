03d0bbf8fda638977bb08e8a560cdd8ec7382588
bilana1990 <giobilana@gmail.com>
2024-06-17 13:50:03 +0400

    Add new files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="gio poto cv.jpg" className="App-logo" alt="logo" />
+        <p>
+          I am <span>Giorgi Bilanishvili</span> Frontend Web Developer
+        </p>
+        <a
+          className="App-link"
+          href="https://bilana1990.github.io/Bilana-s_Portfolio/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit My Portfolio
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
